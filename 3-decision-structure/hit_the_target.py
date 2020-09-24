e01ae399b1127da701dc25ef2e81cab4435d655b
yingliu <yingliu4203@gmail.com>
2020-09-24 15:33:19 -0700

    revise hit target code

--- 3-decision-structure/hit_the_target.py
+++ 3-decision-structure/hit_the_target.py
@@ -1,17 +1,21 @@
 # Hit the Target Game
 import turtle
+import math
 
 # Named constants
 SCREEN_WIDTH = 600    # Screen width
 SCREEN_HEIGHT = 600   # Screen height
+MAX_DISTANCE = math.ceil(math.sqrt(SCREEN_HEIGHT ** 2 + SCREEN_HEIGHT ** 2) / 2)
+
 TARGET_LLEFT_X = 100  # Target's lower-left X
 TARGET_LLEFT_Y = 250  # Target's lower-left Y
 TARGET_WIDTH = 25     # Width of the target
-FORCE_FACTOR = 30     # Arbitrary force factor
+
 PROJECTILE_SPEED = 1  # Projectile's animation speed
+
+EAST = 0              # Angle of east direction
 NORTH = 90            # Angle of north direction
 SOUTH = 270           # Angle of south direction
-EAST = 0              # Angle of east direction
 WEST = 180            # Angle of west direction
 
 # Setup the window.
@@ -41,11 +45,8 @@ pen.showturtle()
 pen.speed(PROJECTILE_SPEED)
 
 # Get the angle and force from the user.
-angle = turtle.numinput("Input","Enter the projectile's angle", minval=0, maxval=360)
-force = turtle.numinput("Input", "Enter the launch force (1-10)", minval=1, maxval=10)
-
-# Calculate the distance.
-distance = force * FORCE_FACTOR
+angle = int(input("Enter the projectile's angle 0-360: "))
+distance = int(input(f'Enter the launch distance (1-{MAX_DISTANCE}): '))
 
 # Set the heading.
 pen.setheading(angle)
@@ -58,12 +59,16 @@ xcor = pen.xcor()
 ycor = pen.ycor()
 
 # Did it hit the target?
-if (xcor >= TARGET_LLEFT_X and
-    xcor <= (TARGET_LLEFT_X + TARGET_WIDTH) and
-    ycor >= TARGET_LLEFT_Y and
-    ycor <= (TARGET_LLEFT_Y + TARGET_WIDTH)):
-        print('Target hit!')
+is_in_x = ((xcor >= TARGET_LLEFT_X) and 
+    (xcor <= (TARGET_LLEFT_X + TARGET_WIDTH)))
+is_in_y = ((ycor >= TARGET_LLEFT_Y) and
+    (ycor <= (TARGET_LLEFT_Y + TARGET_WIDTH)))
+is_hit = is_in_x and is_in_y
+
+# show message
+if is_hit:
+    print('Target hit!')
 else:
-        print('You missed the target.')
+    print('You missed the target.')
 
 turtle.done()
